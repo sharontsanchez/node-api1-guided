88f421658691a1ef3824039d231c8ce1045f0ea3
Sharon Sanchez <sstharakan@gmail.com>
2022-02-07 23:21:29 -0600

    setting up server.js

--- api/server.js
+++ api/server.js
@@ -1,104 +1,33 @@
-const express = require('express');
-const dogModel = require('./dog-model');
-const server = express();
+// IMPORTS AT THE TOP
+// IMPORTS AT THE TOP
+// IMPORTS AT THE TOP
 
-server.use(express.json());
+const express = require('express') // look inside node modules folder and check for express. 
 
-server.get('/', (req, res) => {
-    console.log('received get request!');
-    res.json("hello world!");
-});
 
-server.get('/api/dogs', (req, res) => {
-    dogModel.findAll()
-        .then(dogs => {
-            res.json(dogs);
-        })
-        .catch(() => {
-            res.status(500).json({ message: "could not get the dogs!" });
-        });
-});
 
-server.get('/api/dogs/:id', (req, res) => {
-    let { id } = req.params;
-    dogModel.findById(id)
-        .then(dog => {
-            console.log(dog);
-            if(dog == null) {
-                res.status(404).json({ message: `dog ${id} not found!` });
-            } else {
-                res.json(dog);
-            }
-        })
-        .catch(() => {
-            res.status(500).json({ message: `could not get dog!` });
-        });
-})
+// INSTANCE OF EXPRESS APP
+// INSTANCE OF EXPRESS APP
+// INSTANCE OF EXPRESS APP
 
-server.post('/api/dogs', (req, res) => {
-    let body = req.body;
-    if(!body.name) {
-        res.status(500).json({ message: `name is required` });
-    } else if(!body.weight) {
-        res.status(500).json({ message: `weight is required` });
-    } else {
-        dogModel.create(body)
-            .then(dog => {
-                res.status(201).json(dog);
-            })
-            .catch(() => {
-                res.status(500).json({ message: `could not create dog!` });
-            });
-    }
-});
+// build a simplistic server, invoking express creates the server for us. 
+const server = express()
 
-server.put('/api/dogs/:id', async (req, res) => {
-    let { id } = req.params;
-    try {
-        // let dog = await dogModel.findById(id);
-        // if(dog == null) {
-        //     res.status(404).json({ message: `dog ${id} not found!` });
-        //     return;
-        // }
 
-        let body = req.body;
-        if(!body.name) {
-            res.status(500).json({ message: `name is required` });
-            return;
-        } else if(!body.weight) {
-            res.status(500).json({ message: `weight is required` });
-            return;
-        } else {
-            let newDog = await dogModel.update(id, body);
-            console.log(newDog);
-            if(newDog == null) {
-                res.status(404).json({ message: `dog ${id} not found!` });
-                return;
-            } else {
-                res.status(200).json(newDog);
-            }
-        }
-    } catch(e) {
-        res.status(500).json({ message: `could not update dog!` });
-    }
-});
 
-server.delete('/api/dogs/:id', (req, res) => {
-    let { id } = req.params;
-    dogModel.delete(id)
-        .then(dog => {
-            if(dog == null) {
-                res.status(404).json({ message: `dog ${id} not found!` });
-                return;
-            }
+// GLOBAL MIDDLEWARE
+// GLOBAL MIDDLEWARE
+// GLOBAL MIDDLEWARE
 
-            res.status(200).json(dog);
-        })
-        .catch(() => {
-            res.status(500).json({ message: `could not delete dog!` });
-        });
-});
+// ENDPOINTS 
+// ENDPOINTS 
+// ENDPOINTS 
 
-// [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
+// build an endpoint 
+server.get('/hello', (req, res) => {
+    res.json({ message: 'hello'})
+}) // pass a callback 
 
-module.exports = server;
+
+// EXPOSING THE SERVER TO OTHER MODULES
+module.exports = server
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // import the server and start it 
 
-console.log('hello world!!!')
+// console.log('hello world!!!')
+
+const server = require('./api/server')
+
+// start the server by server.listen followed by PORT number and success callback
+server.listen(5000, () => {
+    console.log('server listening on 5000')
+})
+
